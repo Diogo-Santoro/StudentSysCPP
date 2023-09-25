7884c6bfc1f5238a81f7bfee80837dd1a68c59c9
Diogo-Santoro <diogo.santoro05@gmail.com>
2023-09-24 23:16:58 -0300

    FINALIZED

--- StudentSystem.cpp
+++ StudentSystem.cpp
@@ -1,180 +1,89 @@
 
 #include<iostream>
 using namespace std;
-string id[30],nome[30],classe[30],curso[30];
 
-int total=0;
-void enter()
-{
-int x=0;
-cout<<"Quantos estudantes deseja??"<<endl;
-cin>>x;
-if(total==0)
-{
-total=x+total;
-for(int i=0;i<x;i++)
-{
-cout<<"\nDados do Estudante "<<i+1<<endl<<endl;
-cout<<"Insira o identificador "<<endl;
-cin>>id[i];
-cout<<"Insira o nome: "<<endl;
-cin>>nome[i];
-cout<<"Insira a classe: "<<endl;
-cin>>classe[i];
-cout<<"Insira o curso: "<<endl;
-cin>>curso[i];
-}
-}
-else
-{
-for(int i=total;i<x+total;i++)
+typedef struct{
+    int id;
+    char nome;
+    char rua;
+    char bairro;
+    char cidade;
+    char estado;
+    int cep;
+    int celular;
+    char email;
+    int cpf;
+    int rg;
 
-{
-cout<<"\nDados do estudante "<<i+1<<endl<<endl;
-cout<<"Insira o identificador ";
-cin>>id[i];
-cout<<"Insira o nome: ";
-cin>>nome[i];
-cout<<"Insira a classe: ";
-cin>>classe[i];
-cout<<"insira o curso: ";
-cin>>curso[i];
+} informacoes;
+
+informacoes cadastro;
+
+void cadastroInfo(){
+    system("cls");
+    cout << "--Cadastro de informações--" << endl << endl;
+    cout << "Código do usuário";
+    cin >> cadastro.id;
+    cout << "Nome do usuário";
+    cin >> cadastro.nome << endl;
+    cout << "--Endereço do usuário--" << endl;
+    cout << "Rua";
+    cin >> cadastro.rua;
+    cout << "Bairro";
+    cin >> cadastro.bairro;
+    cout << "Cidade";
+    cin >> cadastro.cidade;
+    cout << "Estado";
+    cin >> cadastro.estado;
+    cout << "CEP";
+    cin >> cadastro.cep;
+    cout << "--Informações de contato do usuário--" << endl;
+    cout << "Celular";
+    cin >> cadastro.celular;
+    cout << "E-mail";
+    cin >> cadastro.email;
+    cout << "CPF";
+    cin >> cadastro.cpf;
+    cout << "RG";
+    cin >> cadastro.rg;
 }
-total=x+total; 
+
+void listaInfo(){
+    system("cls");
+    cout << endl;
+    cout << "--Listagem de dados--";
+    cout << endl;
+    cout << "Código do cadastro       " << cadastro.id << endl;;
+    cout << "Nome do usuário          " << cadastro.nome << endl;
+    cout << "Rua          " << cadastro.rua << endl;
+    cout << "Bairro          " << cadastro.bairro << endl;
+    cout << "Cidade          " << cadastro.cidade << endl;
+    cout << "Estado          " << cadastro.estado << endl;
+    cout << "CEP          " << cadastro.cep << endl;
+    cout << "Celular          " << cadastro.celular << endl;
+    cout << "E-mail          " << cadastro.email << endl;
+    cout << "CPF          " << cadastro.cpf << endl;
+    cout << "RG          " << cadastro.rg << endl;
+    cout << endl;
+    system("pause");
 }
-}
-void show()
-{
-if(total==0)
-{
-cout<<"Nenhum dado inserido"<<endl;
-}
-else
-{
-for(int i=0;i<total;i++)
-{
-cout<<"\nDados do estudante "<<i+1<<endl<<endl;
-cout<<"Identificador "<<id[i]<<endl;
-cout<<"Nome "<<nome[i]<<endl;
-cout<<"Classe "<<classe[i]<<endl;
-cout<<"Curso "<<curso[i]<<endl;
-}
-}
-}
-void search()
-{
-if(total==0)
-{
-cout<<"Nenhum dado inserido"<<endl;
-}
-else
-{
-string identificaEstudante;
-cout<<"Insira o identificador do estudante"<<endl;
-cin>>identificaEstudante;
-for(int i=0;i<total;i++)
-{
-if(identificaEstudante==id[i])
-{
-cout<<"Identificador "<<id[i]<<endl;
-cout<<"Nome "<<nome[i]<<endl;
-cout<<"Classe "<<classe[i]<<endl;
-cout<<"Curso "<<curso[i]<<endl;
-}
-}
-}
-}
-void update()
-{
-if(total==0)
-{
-cout<<"Nenhum dado inserido"<<endl;
-}
-else{
-string rollno;
-cout<<"Insira o identificador do estudante que deseja alterar"<<endl;
-cin>>identificaEstudante;
-for(int i=0;i<total;i++)
-{
-if(identificaEstudante==id[i])
-{
-cout<<"\nDado anterior"<<endl<<endl;
-cout<<"Dado do estudante "<<i+1<<endl;
-cout<<"Identificador: "<<roll_no[i]<<endl;
-cout<<"Nome "<<nome[i]<<endl;
-cout<<"Classe "<<classe[i]<<endl;
-cout<<"Curso "<<curso[i]<<endl;
-cout<<"\nInsira os novos dados"<<endl<<endl;
-cout<<"Insira o Identificador ";
-cin>>id[i];
-cout<<"Insira o nome: ";
-cin>>nome[i];
-cout<<"Insira a classe: ";
-cin>>classe[i];
-cout<<"Insira o curso: ";
-cin>>curso[i];
-}
-}
-}
-}
-void Delete()
-{
-if(total==0)
-{
-cout<<"Ainda não há dado inserido"<<endl;
-}
-else
-{
-int a;
-cout<<"Tem certeza que irá deletar os dados?"<<endl;
-cout<<"Selecione 1 para deletar"<<endl;
-cin>>a;
-if(a==1)
-{
-total=0;
-cout<<"Todo o dado foi deletado..!!"<<endl;
-}
-else
-{
-cout<<"Por favor aperte 1 para deletar o dado"<<endl;
-}
-}
-}
-int main() 
-{
-int valor;
-while(true) 
-{
-cout<<"\nAperte 1 para inserir dados"<<endl<<endl;
-cout<<"Aperte 2 para mostrar dados"<<endl<<endl;
-cout<<"Aperte 3 para procurar dados"<<endl<<endl;
-cout<<"Aperte 4 para alterar dados"<<endl<<endl;
-cout<<"Aperte 5 para deletar dados"<<endl<<endl;
-cout<<"Aperte 6 para sair"<<endl<<endl;
-cin>>valor;
-switch(valor)
-{
-case 1:
-enter();
-break;
-case 2:
-show();
-break;
-case 3:
-search();
-break;
-case 4:
-update();
-break;
-case 5:
-Delete();
-break;
-case 6:
-exit(0);
-break;
-default:
-cout<<"insersão inválida"<<endl;
-break;
-}
-}
+
+int main() {
+    
+    int op = 1;
+    while (op != 0){
+        system("cls");
+        cout << "1 - Cadastrar"<<endl;
+        cout << "0 - fim"<<endl;
+        cout << "Opcao ";
+        cin >> op;
+        switch (op){
+        case 1 :
+            cadastroInfo();break;
+        case 2 :
+            listaInfo();break;
+        default:break;
+        }
+    }
+    return 0;
 }
